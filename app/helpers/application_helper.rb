6de29a1c403a96d54e63072a7f5c4e9ac1c7977d
Tim Green <tiimgreen@gmail.com>
2014-10-04 21:00:16 +0100

    views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Short method for checking if on certain page
+  def on_page(given_controller_name, given_action_name)
+    controller.controller_name == given_controller_name &&
+    controller.action_name == given_action_name
+  end
+
+  def header_class
+    on_page('home', 'index') ? 'navbar navbar-inverse hero':  'navbar navbar-inverse normal'
+  end
 end
